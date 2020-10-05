147d1b1489ba2743fad6ffbf139c4c210039ea73
MiriamGlez <miriamgonzalez16181@gmail.com>
2020-10-05 15:32:57 -0500

    Hola Mundo
    Aguegué mi nombre, modificando el original

---
+++ Hello world.c
@@ -0,0 +1,12 @@
+#include<stdio.h> //stdio -> Standard Input/Output
+
+int main(){
+	/*My first
+	 * Program
+	 * in C
+	 */
+	printf("Hello World! es: ");
+	printf("Gonzalez Sanchez Miriam Aketzali");
+	//Comment
+	return 0;
+}
